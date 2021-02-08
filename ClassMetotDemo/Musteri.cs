12c387533896e7464c395955f2eb63e5d9cdfa5c
haliseseyda <77547091+haliseseyda@users.noreply.github.com>
2021-02-08 03:40:28 +0300

    08.02.2021

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int MusteriId { get; set; }
+        public string MUsteriAdi { get; set; }
+        public string MusteriSoyadi { get; set; }
+        public int MusteriYas { get; set; }
+    }
+}
---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MUsteriAdi + "Eklendi.");
+        }
+
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MUsteriAdi + "Güncellendi.");
+        }
+    }
+}
