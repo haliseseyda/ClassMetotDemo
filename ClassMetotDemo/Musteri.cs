35cc5e2ef6480e5d9919e5a8cf828ff2941de5f2
haliseseyda <77547091+haliseseyda@users.noreply.github.com>
2021-02-08 03:43:21 +0300

    08.02.2021

--- ClassMetotDemo/Musteri.cs
+++ ClassMetotDemo/Musteri.cs
@@ -7,7 +7,7 @@ namespace ClassMetotDemo
     class Musteri
     {
         public int MusteriId { get; set; }
-        public string MUsteriAdi { get; set; }
+        public string MusteriAdi { get; set; }
         public string MusteriSoyadi { get; set; }
         public int MusteriYas { get; set; }
     }
